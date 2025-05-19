6258761a599ff8fb9e53bd49af66e3039c1d643f
SanjaCub <sanja.buha@gmail.com>
2025-05-19 12:39:50 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router"
-import Loader from "./Components/Loader"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router";
+import AllDrivers from "./Components/AllDrivers";
+import AllTeams from "./Components/AllTeams";
+import AllRaces from "./Components/AllRaces";
 
-function App() {
+export default function App() {
   return (
-    <>
-   <Loader/>
-    </>
-  )
+    <Router>
+
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li><Link to="/drivers">All drivers</Link></li>
+          <li><Link to="/teams">All teams</Link></li>
+          <li><Link to="/races">All races</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/drivers" element={<AllDrivers />} />
+        <Route path="/teams" element={<AllTeams />} />
+        <Route path="/races" element={<AllRaces />} />
+      </Routes>
+
+    </Router>
+  );
 }
 
-export default App
+
