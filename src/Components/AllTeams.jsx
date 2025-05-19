2a1ef49fbc498ed1e05ed1c587edcc8ab33f681f
lenkaist <lenka.istijanovic@gmail.com>
2025-05-19 13:40:16 +0200

    Dodata stranica AllTeams

--- src/Components/AllTeams.jsx
+++ src/Components/AllTeams.jsx
@@ -1,7 +1,54 @@
-export default function AllTeams() {
+import { useState, useEffect } from "react";
+import axios from "axios";
+// import Loader form "./components/Loader"
 
-    return(
+export default function App() {
+
+    const [allTeams, setAllTeams] = useState([]);
+    // const [isLoading, setIsLoading] = useState (true);
+
+    useEffect(() => {
+        getAllTeams();
+    }, []);
+
+    const getAllTeams = async () => {
+        const response = await axios.get("http://ergast.com/api/f1/2013/constructorStandings.json");
+        console.log(response.data);
+
+        setAllTeams(response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
+    };
+
+
+    // if (isLoading) {
+    //   return <Loader />;
+    // }
+
+
+    return (
         <>
+            <div><h1>Constructors Championship</h1></div>
+            <table>
+
+                <thead>
+                    <tr>
+                        <th colSpan={4}>Constructors Championship Standings - 2013</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {allTeams.map((team) => {
+                        return (
+                            <tr key={team.position}>
+                                <td>{team.position}</td>
+                                <td>{team.Constructor.nationality} {team.Constructor.name}</td>
+                                <td>Details {team.Constructor.url}</td>
+                                <td>{team.points}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+
+            </table>
         </>
-    );
+    )
 }
